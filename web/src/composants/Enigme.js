6f005fd2263c3a75edd5c5cae4875d285a0d03e4
hugoodn <hugo.donne@ynov.com>
2023-02-13 17:54:19 +0100

    last modif front Enigme

--- web/src/composants/Enigme.js
+++ web/src/composants/Enigme.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import fouras from '../data/fouras.gif';
-import '../css/Enigme.css'
-
+import '../css/Enigme.css';
 
 function Enigme() {
   return (
     <div className="Enigme">
-            <img src={fouras} className="Enigme-fouras" alt="fouras" />
-            <p className='texte'>
-            Plus j'ai de gardiens,<br/>
-            Moins je suis gardé. <br/>
-            Moins j'ai de gardiens, <br/>
-            Plus je suis gardé. <br/>
-            Qui est-il ?    <br/>
-            </p>
-             <label className="answer" for="answer">Entrer votre réponse </label>
-             <input type="text" answer="answer"></input>
-     </div>
+      <div className="images">
+        <img src={fouras} className="Enigme-fouras" alt="fouras" />
+      </div>
+      <div className="user">
+        <h2 className='texte'>
+          Plus j'ai de gardiens,<br />
+          Moins je suis gardé. <br />
+          Moins j'ai de gardiens, <br />
+          Plus je suis gardé. <br />
+          Qui est-il ?    <br />
+        </h2>
+        <label className="answer" for="answer">Entrer votre réponse </label>
+        <input className="falseUsers" type="text" answer="answer"></input>
+        <input className="trueUsers" type="text" answer="answer"></input>
+      </div>
+    </div>
   );
 }
 
