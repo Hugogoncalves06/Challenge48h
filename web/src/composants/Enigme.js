9b3cd7fad6626f425d1924b2505e64a5b95b4dde
ludogernay <ludovic.gernay@ynov.com>
2023-02-13 15:54:54 +0100

    etape enigme front

---
+++ web/src/composants/Enigme.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import fouras from '../data/fouras.gif';
+import '../css/Enigme.css'
+
+
+function Enigme() {
+  return (
+    <div className="Enigme">
+            <img src={fouras} className="Enigme-fouras" alt="fouras" />
+            <p className='texte'>
+            Plus j'ai de gardiens,<br/>
+            Moins je suis gardé. <br/>
+            Moins j'ai de gardiens, <br/>
+            Plus je suis gardé. <br/>
+            Qui est-il ?    <br/>
+            </p>
+             <label className="answer" for="answer">Entrer votre réponse </label>
+             <input type="text" answer="answer"></input>
+     </div>
+  );
+}
+
+export default Enigme;
--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './composants/App';
+//import Start from './composants/Start';
 import reportWebVitals from './composants/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
