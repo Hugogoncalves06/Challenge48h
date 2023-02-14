250a5c1643bc2e31654bf1f990f034b1eead0ba6
chris <chris@DESKTOP-BO1QB81>
2023-02-14 10:55:39 +0100

    Ajout des jeux unity

--- web/src/composants/Snake.js
+++ web/src/composants/Snake.js
@@ -1,11 +1,28 @@
-import '../css/Snake.css'
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import '../css/Tir.css'
 
-function Snake(){
-    return(
-        <div className='snake'>
-            <p className='texte'>Emplacement <br/>du<br/> jeu</p>
-        </div>
-    )
-}
+function Snake() {
+    const { unityProvider } = useUnityContext({
+      loaderUrl: "./Snake/build/Snake.loader.js",
+      dataUrl: "./Snake/build/Snake.data",
+      frameworkUrl: "./Snake/build/Snake.framework.js",
+      codeUrl: "./Snake/build/Snake.wasm",
+    });
 
-export default Snake
+    return (
+        <div className="Tir">
+            <h1>Snake games</h1>            
+            <Unity 
+            unityProvider={unityProvider} 
+            style = {{
+                idth: "1101", 
+                height: "515",
+                border: "2px solid black",
+                background: "green"}}
+            />
+        </div> 
+    );
+  }
+
+  export default Snake;
