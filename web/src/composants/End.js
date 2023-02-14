fc11825900895512a95a9a89c7ab303515748b53
Hugogoncalves06 <hugo.goncalves06@ynov.com>
2023-02-14 09:57:27 +0100

    End paragraphe updated

--- web/src/composants/End.js
+++ web/src/composants/End.js
@@ -6,8 +6,10 @@ function Fin(){
         <div>
             <img src={bravo} className='bravo' alt='fel'/>
             <p>Enfin, c'est pas trop tôt !!!<br/>
-            Vous avez réussi en ..min..sec<br/>
-            Le petit Kevin en CE2 à réussi en 5 min
+            Vous avez réussi en 15 min ...<br/>
+            Le petit Kevin en CE2 de l'école <br/>
+            Roger Cerclier de Guéret<br/>
+            à réussi en 5 min ...
             </p>
         </div>
     )
